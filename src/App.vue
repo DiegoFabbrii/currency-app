5a9de20efe506d87f0f3b32541d36a761a6c0317
Diego Fabbri <diegofabbri@outlook.com>
2022-04-22 18:43:39 -0300

    finished project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Cotação de moedas</h1>
+    <Table :coins="coins" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { toRefs, reactive, onMounted } from "vue";
+import axios from "axios";
+import Table from "./components/Table.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+
+    components: {
+        Table,
+    },
+
+    setup() {
+        const data = reactive({
+            coins: {},
+        });
+
+        onMounted(async () => {
+            const response = await axios.get(
+                "https://economia.awesomeapi.com.br/json/all"
+            );
+
+            data.coins = response.data;
+            console.log(data.coins);
+        });
+
+        return { ...toRefs(data) };
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap");
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "Poppins", sans-serif;
+}
+
+h1 {
+    margin-top: 70px;
+    text-align: center;
 }
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,92 @@
+<template>
+    <main class="container">
+        <table class="container__table">
+            <thead class="table__header">
+                <tr>
+                    <th>Código</th>
+                    <th>Nome</th>
+                    <th>Compra</th>
+                    <th>Venda</th>
+                    <th>Máximo</th>
+                    <th>Mínimo</th>
+                    <th>Variação</th>
+                </tr>
+            </thead>
+
+            <tbody class="table__body">
+                <tr v-for="(coin, key) in coins" :key="key">
+                    <td>{{ coin.code }}</td>
+                    <td>{{ coin.name }}</td>
+                    <td>{{ coin.bid }}</td>
+                    <td>{{ coin.ask }}</td>
+                    <td>{{ coin.high }}</td>
+                    <td>{{ coin.low }}</td>
+                    <td>
+                        <span
+                            class="table__body--pctChange"
+                            :class="{
+                                'table__body--pctChangeHigh':
+                                    coin.pctChange > 0,
+
+                                'table__body--pctChangeLow': coin.pctChange < 0,
+                            }"
+                            >{{ `${coin.pctChange} %` }}</span
+                        >
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Table",
+    props: {
+        coins: Object,
+    },
+};
+</script>
+
+<style scoped>
+.container {
+    overflow-x: auto;
+    width: 80vw;
+}
+
+.container__table {
+    margin: 70px 0;
+    width: 100%;
+}
+
+.table__body tr {
+    border-bottom: 1px solid #333;
+}
+
+.table__header tr th,
+.table__body tr td {
+    padding: 20px;
+    text-align: start;
+    border-bottom: 1px solid #ccc;
+}
+
+.table__body {
+    font-size: 1rem;
+}
+
+.table__body--pctChange {
+    padding: 3px 7px;
+    border-radius: 10px;
+    background-color: #ddd;
+}
+
+.table__body--pctChangeHigh {
+    background-color: rgb(37, 180, 80);
+    color: #f7f7f7;
+}
+
+.table__body--pctChangeLow {
+    background-color: rgb(190, 71, 63);
+    color: #f7f7f7;
+}
+</style>
